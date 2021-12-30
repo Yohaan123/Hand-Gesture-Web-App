fbd88384f494fffd1bfa55fcfb35372e523ac320
Yohaan <80580077+Yohaan123@users.noreply.github.com>
2021-12-30 17:37:22 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -16,6 +16,11 @@ function take_snapshot(){
     });
 
 }
+
+function check(){
+    
+}
+
 console.log("ml5 version", ml5.version);
 
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZfQIXTmNC/model.json',modelLoaded);
